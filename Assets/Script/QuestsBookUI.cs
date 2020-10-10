d24641830a7a83acae7670ee752cc391b2c6b973
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-10 20:09:49 +0300

    Primary Quest Book UI

---
+++ Assets/Script/QuestsBookUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestsBookUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject questsBookUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        questsBookUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuestsBookUIToogle(){
+        questsBookUI.SetActive(!questsBookUI.activeSelf);
+    }
+}
