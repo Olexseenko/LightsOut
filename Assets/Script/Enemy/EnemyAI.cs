b29206191eee1ec9204993e829f440d4e4b2bbed
Olexseenko <olekseenko13@gmail.com>
2020-10-12 13:23:52 +0300

    Add navigation

--- Assets/Script/Enemy/EnemyAI.cs
+++ Assets/Script/Enemy/EnemyAI.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAI : MonoBehaviour
 {
     public CharacterController controller;
@@ -9,31 +11,125 @@ public class EnemyAI : MonoBehaviour
     private Rigidbody rigidbody;
     private Vector3 startingPosition;
     private Vector3 roamPosition;
+    private NavMeshAgent agent;
+
+    public enum State { 
+        PATROL,
+        CHASE
+    }
+
+    public State state;
+    private bool alive;
+
+    // Vars for Patrol
+    public GameObject[] waypoints;
+    public int wayPointInd = 0;
+    public float patrolSpeed = 0.5f;
+
+
+    // Vars for Chase
+    public float chaseSpeed = 1f;
+    public GameObject target;
 
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Start()
     {
         startingPosition = transform.position;
         roamPosition = GetRoamingPosition();
+
+        agent.updatePosition = true;
+        agent.updateRotation = false;
+
+        state = State.PATROL;
+
+        alive = true;
+
+        StartCoroutine(FSM());
     }
 
     void Update()
     {
-        rigidbody.velocity = roamPosition * speed;
+        //switch (state)
+        //{
+        //    case State.PATROL:
+        //        Patrol();
+        //        break;
+        //    case State.CHASE:
+        //        break;
+        //    default:
+        //        break;
+        //}
 
-        FindTarget();
+        //MoveToPoint(roamPosition);
 
-        float reachedPositionDistance = 1f;
-        if (Vector3.Distance(transform.position, roamPosition) < reachedPositionDistance)
+
+        //FindTarget();
+
+        //float reachedPositionDistance = 1f;
+        //if (Vector3.Distance(transform.position, roamPosition) < reachedPositionDistance)
+        //{
+        //    roamPosition = GetRoamingPosition();
+        //}
+
+    }
+
+    IEnumerator FSM()
+    {
+        while (alive)
         {
-            roamPosition = GetRoamingPosition();
-        }
+            switch (state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+                case State.CHASE:
+                    Chase();
+                    break;
+                default:
+                    break;
+            }
 
+            yield return null;
+        }
+    }
+
+    void Patrol()
+    {
+        agent.speed = patrolSpeed;
+
+        if (Vector3.Distance(this.transform.position, waypoints[wayPointInd].transform.position) >= 1)
+        {
+            agent.SetDestination(waypoints[wayPointInd].transform.position);
+        }
+        else
+        {
+            wayPointInd++;
+            if (waypoints.Length <= wayPointInd)
+            {
+                wayPointInd = 0;
+            }
+        }
+    }
+
+    void Chase() 
+    {
+        agent.speed = chaseSpeed;
+        agent.SetDestination(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            state = State.CHASE;
+            target = other.gameObject;
+        }
     }
 
     private Vector3 GetRoamingPosition()
@@ -50,5 +146,10 @@ public class EnemyAI : MonoBehaviour
         {
             roamPosition = PlayerController.PlayerPosition;
         }
+    }
+
+    private void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
     }
 }
