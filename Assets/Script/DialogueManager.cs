54bcbbffc7e16582a447f718473cb16009973910
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-14 21:25:21 +0300

    v1.10142125
    - Add test dialogue system
    - Create prototype Prolog lvl

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text targetDialogue;
+
+    [SerializeField]
+    private Text dialogueText;
+
+    [SerializeField]
+    private GameObject dialogueUI;
+
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(DialoguePattern dialogue)
+    {
+        dialogueUI.SetActive(true);
+        targetDialogue.text = dialogue.dialogueTargeter;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }   
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialoge();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        //dialogueText.text = sentence;
+
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialoge()
+    {
+        dialogueUI.SetActive(false);
+        
+    }
+}
---
+++ Assets/Script/DialogueTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public DialoguePattern dialogue;
+    
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
