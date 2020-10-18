e88c82c40d68b67103754c77e811fd1c0b434310
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-18 13:49:40 +0300

    v1.10181350
    - Add main menu

---
+++ Assets/Script/MainMenu/Cow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSorce;
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        audioSorce.Stop();
+        audioSorce.PlayOneShot(audioClip);
+    }
+}
