663f46f1cdb9d2a9779a166b2155a12d573ed1f4
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-21 13:45:16 +0300

    v1.10211345
    - Add portal sound
    - Add hit sound
    - Add fight music
    - Add transition

---
+++ Assets/Script/MainMenu/LvlLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlLoader : MonoBehaviour
+{
+    public Animator animator;
+
+    public float transitionTime = 1f;
+
+    public void LoadNextLvl()
+    {
+        StartCoroutine(LoadLvl(SceneManager.GetActiveScene().buildIndex + 1));
+        
+    }
+
+    IEnumerator LoadLvl(int levelIndex)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+
+    }
+    
+    
+}
---
+++ Assets/Script/Sound/SwitchSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSound : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SoundManager.instance.state = SoundManager.State.PART_2;
+            SoundManager.instance.CurrentPlay();
+        }
+    }
+}
