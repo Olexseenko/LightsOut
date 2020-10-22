ad818dbeeb2a5906caa03b35081d661d719a19eb
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-10-22 14:26:14 +0300

    v1.10221426
     - Add wait triggers on some dialog
     - Fixed distanse to sphere and key

---
+++ Assets/Script/DialogueWait.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueWait : MonoBehaviour
+{
+    public float timeToWait = 4f;
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {   
+            StartCoroutine(WaitDialog());
+        }
+    }
+
+    private IEnumerator WaitDialog()
+    {   
+        playerMovement.state = PlayerMovement.State.hide;
+
+        yield return new WaitForSeconds(timeToWait);
+
+        playerMovement.state = PlayerMovement.State.normal;
+
+        Destroy(gameObject);
+    }
+}
